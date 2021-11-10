cb605b6a9bd9874bc9881a2275931c1f0bcc3d4a
leonidamaltsev <daf.dof@yandex.ru>
2021-11-10 15:01:54 +0300

    6.9. Жадный алгоритм. Сдача в кофе машине [#34741 #226603]

--- src/main/java/ru/job4j/array/MatrixCheck.java
+++ src/main/java/ru/job4j/array/MatrixCheck.java
@@ -4,8 +4,30 @@ public class MatrixCheck {
     public static boolean monoHorizontal(char[][] board, int row) {
         boolean result = true;
         for (int i = 0; i < board.length; i++) {
-            if (board[row][row] != 'X') {
+            if (board[row][i] != 'X') {
                 result = false;
+                break;
+            }
+        }
+        return result;
+    }
+
+    public static boolean monoVertical(char[][] board, int column) {
+        boolean result = true;
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][column] != 'X') {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+
+    public static boolean isWin(char[][] board) {
+        boolean result = false;
+        for (int i = 0; i < board.length; i++) {
+            if ((board [i][i] == 'X') && (monoHorizontal(board, i) || monoVertical(board, i))) {
+                result = true;
                 break;
             }
         }
